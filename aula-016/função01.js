c0878dc53fad2e609c6f3a73356c1b88b4681f26
Ewerson de Jesus <71734681+Ewerson2@users.noreply.github.com>
2021-03-07 18:01:32 -0300

    Delete aula-016 directory

--- aula-016/função01.js
+++
@@ -1,9 +0,0 @@
-function parimpar(n) {
-    if(n%2 == 0){
-        return 'Par'
-    } else{
-        return 'Impar'
-    }
-    
-}
-console.log(parimpar(220))
--- aula-016/função02.js
+++
@@ -1,5 +0,0 @@
-function soma(n1, n2=0){
-    return n1 + n2
-}
-
-console.log(soma(2))
